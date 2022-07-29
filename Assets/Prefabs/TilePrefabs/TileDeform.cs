5d4d2e10c146744f24c58248dce6b5618715dd86
Murray Smith <murray.smith@unity3d.com>
2022-07-28 22:22:03 -0400

    Fixed procedural vertex gneeration and added water shader

--- Assets/Prefabs/TilePrefabs/TileDeform.cs
+++ Assets/Prefabs/TilePrefabs/TileDeform.cs
@@ -9,6 +9,9 @@ public class TileDeform : MonoBehaviour
     public float tileSize;
     public int borderSize = 1;
     public float noiseIntensity = 1f;
+    public float minMiddleHeight = 0.1f;
+    public float maxMiddleHeight = 2f;
+    public float yOffset = 1f;
     Mesh mesh;
 
     Vector3[] vertices;
@@ -51,46 +54,69 @@ public class TileDeform : MonoBehaviour
         uv = new Vector2[(xVerticesPlusBorder + 1) * (zVerticesPlusBorder + 1)];
 
 
+        Vector3[][] tempVerts = new Vector3[zVerticesPlusBorder + 1][];
+        int j = 0;
+        for (int z = 0; z <= zVerticesPlusBorder; z++) {
+            tempVerts[z] = new Vector3[xVerticesPlusBorder + 1];
+            for (int x = 0; x <= xVerticesPlusBorder; x++) {
+                float y = Mathf.Clamp(Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * noiseIntensity, minMiddleHeight, maxMiddleHeight);
+                tempVerts[z][x] = new Vector3((x - borderSize) * tileSize, y, (z - borderSize) * tileSize);
+                uv[j] = new Vector2((float)x / xVerticesPlusBorder, (float)z / zVerticesPlusBorder);
+                j++;
+            }
+        }
+
         int i = 0;
         for (int z = 0; z <= zVerticesPlusBorder; z++) {
             for (int x = 0; x <= xVerticesPlusBorder; x++) {
-                float y = Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * noiseIntensity;;
+
+                float y = tempVerts[z][x].y;
+
                 bool xInside = borderSize <= x && x <= xVerticesPlusBorder - borderSize;
                 bool zInside = borderSize <= z && z <= zVerticesPlusBorder - borderSize;
                 if (!xInside || !zInside) {
+                    float yMult = 1f;
                     int xDistanceFromBorder = 0, zDistanceFromBorder = 0;
                     if (x < borderSize) {
                         xDistanceFromBorder = borderSize - x;
                     } 
-                    else {
+                    else if (xVerticesPlusBorder - borderSize < x) {
                         xDistanceFromBorder = x - (xVerticesPlusBorder - borderSize);
                     }
 
                     if (z < borderSize) {
                         zDistanceFromBorder = borderSize - z;
                     } 
-                    else {
+                    else if (zVerticesPlusBorder - borderSize < z) {
                         zDistanceFromBorder = z - (zVerticesPlusBorder - borderSize);
                     }
 
-                    if (!xInside || z == borderSize || z == zVerticesPlusBorder - borderSize) {
-                            y *= 1 - (xDistanceFromBorder / borderSize);
+                    int closestX = x, closestZ = z;
+                    if (!xInside) {
+                        if (x < borderSize) closestX = borderSize;
+                        else closestX = xVerticesPlusBorder - borderSize;
                     }
-                    else if (!zInside || x == borderSize || x == xVerticesPlusBorder - borderSize) {
-                        y *= 1 - (zDistanceFromBorder / borderSize);
+                    if (!zInside) {
+                        if (z < borderSize) closestZ = borderSize;
+                        else closestZ = zVerticesPlusBorder - borderSize;
                     }
-                    else if (!xInside && !zInside)
-                        y *= (1 - ((xDistanceFromBorder + zDistanceFromBorder) / (2 * borderSize)));
 
-                    
+                    if (xDistanceFromBorder == zDistanceFromBorder) {
+                        yMult = 1 - (float)(xDistanceFromBorder * xDistanceFromBorder + zDistanceFromBorder * zDistanceFromBorder) / (2 * borderSize * borderSize);
+                    }
+                    else {
+                        yMult = 1 - ((float)Mathf.Max(xDistanceFromBorder, zDistanceFromBorder) / borderSize);
+                    }
+
+                    Debug.Log($"xDistance: {xDistanceFromBorder}, zDistance: {zDistanceFromBorder}, yMult: {yMult}");
+
+                    y = yMult * tempVerts[closestZ][closestX].y;
                 }
-                else y = Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * noiseIntensity;
-                vertices[i] = (new Vector3(x, y, z) - new Vector3(borderSize, 0, borderSize)) * tileSize;
-                uv[i] = new Vector2((float)x / xVerticesPlusBorder, (float)z / zVerticesPlusBorder);
+
+                vertices[i] = new Vector3(tempVerts[z][x].x, y - yOffset, tempVerts[z][x].z);
                 i++;
             }
         }
-
     }
 
     void UpdateMesh() {
